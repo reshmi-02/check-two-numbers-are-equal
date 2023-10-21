81ccc6157dd7feed1edf2cbae4733c502223e49f
reshmi-02 <reshmihash123@gmail.com>
2023-10-21 18:32:11 +0530

    Addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+
+var a=parseInt(prompt("Enter the number 1"));
+var b=parseInt(prompt("Enter the  number2"));
+
+if(a==b){
+    document.write("number 1 and number 2 are equal");
+}
+else{
+  document.write("number 1 and number 2 are not  equal");
+}
